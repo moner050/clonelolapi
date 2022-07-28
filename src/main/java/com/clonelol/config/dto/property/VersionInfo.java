89d496edafe351e90a681e3fed8a67b6763211a6
moner050 <minhyung0237@gmail.com>
2022-07-28 14:18:22 +0900

    feat: Create VersionInfo

---
+++ src/main/java/com/clonelol/config/dto/property/VersionInfo.java
@@ -0,0 +1,18 @@
+package com.clonelol.config.dto.property;
+
+import lombok.Data;
+
+@Data
+public class VersionInfo {
+
+    private String item;
+    private String rune;
+    private String mastery;
+    private String summoner;
+    private String champion;
+    private String profileicon;
+    private String map;
+    private String language;
+    private String sticker;
+
+}
